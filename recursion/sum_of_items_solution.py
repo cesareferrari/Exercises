ecf7a5ec16e647e42f89b7c86405af7ee45a8cf6
Cesare Ferrari <cesare@maryandferrari.com>
2020-06-08 14:32:17 -0400

    Add sum of items

---
+++ recursion/sum_of_items_solution.py
@@ -0,0 +1,13 @@
+"""
+Sum all the elements of the given array recursively
+"""
+
+def sum_of_items(items):
+    if len(items) == 1:
+        return items[0]
+
+    return items[0] + sum_of_items(items[1:])
+
+
+items = [1, 2, 3, 4, 5]
+print(sum_of_items(items) == 15)  # --> True
